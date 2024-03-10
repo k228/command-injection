7034ad42560bbf543b9e073780f6b40451126b8a
alireza jabbari <alirezajabarikopaie@gmail.com>
2024-03-10 13:11:35 +0330

    user execfile instead

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
-const { exec } = require('child_process');
+const { execFile } = require('child_process');
 const app = express();
 const port = 3000;
 const pug = require('pug');
 const fs = require('fs');
 
 
-// Listen in on root
 app.get('/', async (req, res) => {
     const filename = req.query.filename;
     const text = req.query.text;
     const password = req.query.password;
     if (filename && text && password) {
-        // Run the command with the parameter the user gives us
         fs.writeFileSync(`./files/${filename}.txt`, text);
-        exec(`zip --password ${password} ./files/${filename}.zip ./files/${filename}.txt`, (error, stdout, stderr) => {
-            let output = stdout;
+        execFile(`/usr/bin/zip`, [`--password`,`${password}`, `./files/${filename}.zip`, `./files/${filename}.txt`], (error, stdout, stderr) => {
             if (error) {
                 // If there are any errors, show that
-                output = error;
+                res.send(
+                    pug.renderFile('./pages/index.pug', { output: error })
+                );
             }
             res.download(`./files/${filename}.zip`);
         });
